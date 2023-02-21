0b4feb91d46551230e287eb008b84f0ff4bfa68b
acimkompor <acimkompor@yahoo.com>
2023-02-22 01:18:27 +0800

    update with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,35 @@
-const menuHumberger = document.querySelector(".menu-toggle input");
-const navBarMenu = document.querySelector(".navbar .navbar-collapse");
+// POPUP IMAGE
+function imageZoom() {
+  const imageZoome = document.querySelector(".zoom img");
+  const popup = document.querySelector(".popup-image");
 
-menuHumberger.addEventListener("click", function () {
-  navBarMenu.classList.toggle("slide");
-});
+  imageZoome.addEventListener("click", function () {
+    window.setTimeout(function () {
+      document.querySelector(".popup-image").style.display = "block";
+      document.querySelector(".popup-image img").src =
+        imageZoome.getAttribute("src");
+    }, 0);
+  });
 
-// klik diluar sidebar untuk menghilangkan menu
+  document.querySelector(".popup-image span").onclick = () =>
+    (popup.style.display = "none");
+}
 
-document.addEventListener("click", function (e) {
-  if (!menuHumberger.contains(e.target) && !navBarMenu.contains(e.target)) {
-    navBarMenu.classList.remove("slide");
-    menuHumberger.checked = false;
-  }
-});
+// Humberger menu
+function humberger() {
+  const menuHumberger = document.querySelector(".menu-toggle input");
+  const navBarMenu = document.querySelector(".navbar .navbar-collapse");
+
+  menuHumberger.addEventListener("click", function () {
+    navBarMenu.classList.toggle("slide");
+  });
+
+  // klik diluar sidebar untuk menghilangkan menu
+
+  document.addEventListener("click", function (e) {
+    if (!menuHumberger.contains(e.target) && !navBarMenu.contains(e.target)) {
+      navBarMenu.classList.remove("slide");
+      menuHumberger.checked = false;
+    }
+  });
+}
